4d68712680d16e0e07e06434dce2ca08b620a9cb
Karolina-Kuza8 <163876396+Karolina-Kuza8@users.noreply.github.com>
2025-11-15 14:25:03 +0000

    zadanie 1 zakończone

--- ZADANIE1/zadanie1.py
+++ ZADANIE1/zadanie1.py
@@ -1,51 +1,42 @@
 def dodaj_element(wejscie):
+    # funkcja która znajdzie maksymalną głębokość
+    def znajdz_glebokosc(x, poziom=1):
+        max_g = poziom
+        if isinstance(x, list) or isinstance(x, tuple):
+            for e in x:
+                g = znajdz_glebokosc(e, poziom + 1)
+                if g > max_g:
+                    max_g = g
+        elif isinstance(x, dict):
+            for v in x.values():
+                g = znajdz_glebokosc(v, poziom + 1)
+                if g > max_g:
+                    max_g = g
+        return max_g
 
-    def max_depth(obj, depth=0):
-        """
-        Zwraca maksymalną głębokość list w strukturze.
-        """
-        if isinstance(obj, list):
-            d = depth
-            for el in obj:
-                d = max(d, max_depth(el, depth + 1))
-            return d
-        elif isinstance(obj, tuple):
-            d = depth
-            for el in obj:
-                d = max(d, max_depth(el, depth + 1))
-            return d
-        elif isinstance(obj, dict):
-            d = depth
-            for v in obj.values():
-                d = max(d, max_depth(v, depth + 1))
-            return d
-        else:
-            return depth
+    # funkcja która doda elementy w najbardziej zagłębionych listach
+    def dodaj(x, poziom, maks):
+        if isinstance(x, list):
+            # jeśli to najbardziej zagnieżdżona lista → dodaj element
+            if poziom == maks:
+                # wartość to po prostu kolejny numer (długość + 1)
+                x.append(len(x) + 1)
+            else:
+                for i in range(len(x)):
+                    x[i] = dodaj(x[i], poziom + 1, maks)
+        elif isinstance(x, tuple):
+            # krotki nie można zmieniać, więc tworzę nową
+            nowa = []
+            for e in x:
+                nowa.append(dodaj(e, poziom + 1, maks))
+            return tuple(nowa)
+        elif isinstance(x, dict):
+            for k in x:
+                x[k] = dodaj(x[k], poziom + 1, maks)
+        return x
 
-    max_d = max_depth(wejscie)
-
-    def add_at_depth(obj, depth=0):
-        """
-        Modyfikuje obiekt, dodając nowy element do list na maksymalnej głębokości.
-        """
-        if isinstance(obj, list):
-            if depth == max_d:
-                # Dodaj nowy element: len(list) + 1
-                obj.append(len(obj) + 1)
-
-            for el in obj:
-                add_at_depth(el, depth + 1)
-
-        elif isinstance(obj, tuple):
-            for el in obj:
-                add_at_depth(el, depth + 1)
-
-        elif isinstance(obj, dict):
-            for v in obj.values():
-                add_at_depth(v, depth + 1)
-
-    add_at_depth(wejscie)
-    return wejscie
+    maks_gleb = znajdz_glebokosc(wejscie)
+    return dodaj(wejscie, 1, maks_gleb)
 
 
 if __name__ == '__main__':
@@ -54,4 +45,4 @@ if __name__ == '__main__':
         "hello", 3, [4, 5], 5, (6, (1, [7, 8]))
     ]
     output_list = dodaj_element(input_list)
-    print(output_list)
+    print(input_list)
