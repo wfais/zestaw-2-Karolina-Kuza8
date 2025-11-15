89f3d5b4ab4cd7a109a7d31222d086a7015538fb
Karolina-Kuza8 <163876396+Karolina-Kuza8@users.noreply.github.com>
2025-11-15 14:26:44 +0000

    zadanie1 zakończone

--- ZADANIE1/tests/test_zadanie1.py
+++ ZADANIE1/tests/test_zadanie1.py
@@ -1,61 +1,48 @@
-from ZADANIE1.zadanie1 import dodaj_element
+def dodaj_element(wejscie):
 
-def test_pojedyncza_zagniezdzona_lista():
-    structure = [1, [2, 3], 4]
-    expected = [1, [2, 3, 4], 4]
-    assert dodaj_element(structure) == expected
+    # znajdowanie maksymalnej głębokości zagnieżdżeń LIST
+    def max_glebokosc(x, poziom=1):
+        max_g = poziom
+        if isinstance(x, list):
+            for e in x:
+                g = max_glebokosc(e, poziom + 1)
+                if g > max_g:
+                    max_g = g
+        elif isinstance(x, tuple):
+            for e in x:
+                g = max_glebokosc(e, poziom + 1)
+                if g > max_g:
+                    max_g = g
+        elif isinstance(x, dict):
+            for v in x.values():
+                g = max_glebokosc(v, poziom + 1)
+                if g > max_g:
+                    max_g = g
+        return max_g
 
-def test_wiele_zagniezdzonych_list():
-    structure = [1, [3], [2]]
-    expected = [1, [3, 4], [2, 3]]
-    assert dodaj_element(structure) == expected
+    maks = max_glebokosc(wejscie)
 
-def test_gleboko_zagniezdzona_struktura_z_pustymi_listami():
-    structure = [[], {"a": ()}, [1, 2, 3, 4]]
-    expected = [[1], {"a": ()}, [1, 2, 3, 4, 5]]
-    assert dodaj_element(structure) == expected
+    # dodawanie elementu do list na maksymalnej głębokości
+    def dodaj(x, poziom=1):
+        if isinstance(x, list):
+            if poziom == maks:
+                x.append(len(x) + 1)
+            else:
+                for i in range(len(x)):
+                    x[i] = dodaj(x[i], poziom + 1)
+            return x
 
-def test_struktura_zlozona_z_slownikami_i_krotkami():
-    structure = [
-        1, 2, [3, 4, [5, {"klucz": [5, 6], "tekst": [1, 2]}], 5],
-        "hello", 3, [4, 5], (5, (6, (1, [7, 8])))
-    ]
-    expected = [
-        1, 2, [3, 4, [5, {"klucz": [5, 6, 7], "tekst": [1, 2, 3]}], 5],
-        "hello", 3, [4, 5], (5, (6, (1, [7, 8, 9])))
-    ]
-    assert dodaj_element(structure) == expected
+        elif isinstance(x, tuple):
+            nowa = []
+            for e in x:
+                nowa.append(dodaj(e, poziom + 1))
+            return tuple(nowa)
 
-def test_pusta_najglebsza_lista():
-    structure = [1, 2, [[], {"klucz": [], "tekst": []}], 3]
-    expected = [1, 2, [[], {"klucz": [1], "tekst": [1]}], 3]
-    assert dodaj_element(structure) == expected
+        elif isinstance(x, dict):
+            for k in x:
+                x[k] = dodaj(x[k], poziom + 1)
+            return x
 
-def test_jedyna_pusta_lista():
-    structure = [[]]
-    expected = [[1]]
-    assert dodaj_element(structure) == expected
+        return x
 
-def test_wiele_pustych_list():
-    structure = [[], [[], []]]
-    expected = [[], [[1], [1]]]
-    assert dodaj_element(structure) == expected
-
-def test_zagniezdzone_puste_i_niepuste_listy():
-    structure = [[1, 2], [], [3, [4, []]]]
-    expected = [[1, 2], [], [3, [4, [1]]]]
-    assert dodaj_element(structure) == expected
-
-def test_zlozona_struktura_z_roznych_typow():
-    structure = [
-        "hello", 3.5, [1, 2, [], {"data": [7, 8, []]}], (5, [6, (7, [8, 9])])
-    ]
-    expected = [
-        "hello", 3.5, [1, 2, [], {"data": [7, 8, [1]]}], (5, [6, (7, [8, 9, 10])])
-    ]
-    assert dodaj_element(structure) == expected
-
-def test_bez_zagniezdzenia():
-    structure = [1, 2, 3, 4]
-    expected = [1, 2, 3, 4, 5]  # Dodajemy 5 do jedynej listy
-    assert dodaj_element(structure) == expected
+    return dodaj(wejscie)
